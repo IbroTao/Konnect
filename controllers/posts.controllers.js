0f336baf79cd06ce39345c2421b53f5fffd70916
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-19 11:45:32 +0100

    Post Controller: getSinglePost & getAllPosts

--- controllers/posts.controllers.js
+++ controllers/posts.controllers.js
@@ -66,10 +66,42 @@ const deletePost = async (req, res) => {
   }
 };
 
+const getSinglePost = async (req, res) => {
+  const { id } = req.params;
+  validateMongoId(id);
+  try {
+    const post = await Posts.findById(id);
+    if (!post) {
+      res.status(404).json({ message: "Post not found or already deleted" });
+    }
+
+    res.status(200).json(post);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  const posts = await Posts.find().sort({
+    createdAt: "desc",
+  });
+  try {
+    if (!post) {
+      res.status(404).json({ message: "Posts not found" });
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 const likePost = async (req, res) => {};
 
 module.exports = {
   createPost,
   editPost,
   deletePost,
+  getSinglePost,
+  getAllPosts,
 };
