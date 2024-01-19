f1a854b0740fa64e7cb1c832ad1280ff2f9b7a24
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-19 07:05:29 +0100

    User Controller: sendResetPasswordTokenViaEmail, updatePasswordViaEmail

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,10 +2,12 @@ const { Users } = require("../models/user.models");
 const {
   sendPasswordResetTokenEmail,
 } = require("../utils/sendPasswordResetEmail");
+const { validateMongoId } = require("../utils/validateMongoId");
 const crypto = require("crypto");
 
 const updateUserDetails = async (req, res) => {
   const { _id } = req.user;
+  validateMongoId(_id);
   try {
     const user = await Users.findByIdAndUpdate(_id, req.body, {
       new: true,
@@ -29,7 +31,7 @@ const sendResetPasswordTokenViaEmail = async (req, res) => {
 
   const resetToken = crypto.randomBytes(70).toString("hex");
 
-  await sendResetPasswordTokenViaEmail({
+  await sendPasswordResetTokenEmail({
     name: user.name,
     token: resetToken,
   });
@@ -45,4 +47,35 @@ const sendResetPasswordTokenViaEmail = async (req, res) => {
   });
 };
 
-const updatePassword = async (req, res) => {};
+const updatePasswordViaEmail = async (req, res) => {
+  const { password } = req.body;
+  const { token } = req.params;
+  const user = await Users.findOne({
+    passwordResetToken: token,
+    passwordResetTokenExpiresAt: { $gt: Date.now() },
+  });
+  try {
+    if (!user) {
+      res.status(400).json({ message: "Token expired" });
+    }
+
+    user.password = password;
+    user.passwordResetToken = null;
+    user.passwordResetTokenExpiresAt = null;
+    const newUser = await user.save();
+
+    res.status(200).json({ message: "Password updated successfully", newUser });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const updatePassword = async (req, res) => {
+  const { _id } = req.user;
+  validateMongoId(_id);
+  try {
+    const { oldPassword, newPassword } = req.body;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
