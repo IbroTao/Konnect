fee4c1d349531c89cf98498bd51fdde6323df2ad
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-21 00:00:55 +0100

    getGroupMembers cont'd

--- routes/groups.routes.js
+++ routes/groups.routes.js
@@ -22,6 +22,20 @@ const {
   joinGroup,
   likePost,
   makeAdmin,
+  getGroupMembers,
+  getGroupMessages,
+  sendMessage,
 } = require("../controllers/groups.contollers");
+
+router.post("/", authenticateUser, createGroup);
+router.post("/posts/", authenticateUser, createPost);
+router.put("/:id", authenticateAdmin, editGroupDetails);
+router.put("/post/:id", authenticateUser, editPost);
+router.put("/post/approve/:id", authenticateAdmin, approvePost);
+router.get("/post/:id", authenticateUser, getSinglePost);
+router.get();
+router.get("/posts/", authenticateAdmin, getAllPosts);
+router.delete("/:id", authenticateAdmin, deleteGroup);
+router.delete("/post/:id", authenticateUser, deletePost);
 
 module.exports = router;
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,4 +1,25 @@
 const { Router } = require("express");
 const router = Router();
 
+const {
+  authenticateAdmin,
+  authenticateUser,
+  restrictBlockedUsers,
+} = require("../middlewares/header");
+
+const {
+  createPost,
+  addComments,
+  deletePost,
+  dislikePost,
+  editPost,
+  getAllPosts,
+  getSinglePost,
+  likePost,
+} = require("../controllers/posts.controllers");
+
+router.post("/", authenticateUser, restrictBlockedUsers, createPost);
+router.delete("/:id", authenticateUser, deletePost);
+router.put("/:id", authenticateUser, editPost);
+
 module.exports = router;
