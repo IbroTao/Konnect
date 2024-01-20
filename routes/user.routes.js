552a1d3844eabcb95aca6a12a5585c40a9c34cc4
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-20 18:25:56 +0100

    Routes: authRoutes set up

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,15 +2,35 @@ const { Router } = require("express");
 const router = Router();
 
 const {
-  loginUser,
-  logoutUser,
-  signupUser,
-  verifyAndSignupUser,
-} = require("../controllers/auth.controller");
+  authenticateAdmin,
+  authenticateUser,
+  restrictBlockedUsers,
+} = require("../middlewares/header");
 
-router.post("/signup", signupUser);
-router.post("/login", loginUser);
-router.post("/verify-email", verifyAndSignupUser);
-router.get("/logout", logoutUser);
+const {
+  blockUser,
+  deleteUser,
+  getAllUsers,
+  getSingleUser,
+  sendPasswordResetTokenEmail,
+  unblockUser,
+  updatePassword,
+  updatePasswordViaEmail,
+  updateUserDetails,
+} = require("../controllers/user.controllers");
+
+router.post("/reset-password/:token", authenticateUser, updatePasswordViaEmail);
+router.post(
+  "/forgot-password-token",
+  authenticateUser,
+  sendPasswordResetTokenEmail
+);
+router.get("/", authenticateUser, authenticateAdmin, getAllUsers);
+router.get("/:id", authenticateUser, restrictBlockedUsers, getSingleUser);
+router.put("/block/:id", authenticateAdmin, blockUser);
+router.put("/unblock/:id", authenticateAdmin, unblockUser);
+router.put("/passowrd", authenticateUser, updatePassword);
+router.put("/:id", authenticateUser, updateUserDetails);
+router.delete("/:id", authenticateUser, deleteUser);
 
 module.exports = router;
