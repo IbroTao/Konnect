437c780b67291d2bcc6610d59eb0ae008d546a1a
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-21 00:06:43 +0100

    Routes: chatRoutes set up

--- routes/chats.routes.js
+++ routes/chats.routes.js
@@ -1,4 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
+const {
+  authenticateAdmin,
+  authenticateUser,
+  restrictBlockedUsers,
+} = require("../middlewares/header");
+
 module.exports = router;
