95cfef01ab9a5e797cdbb182ab352d612041aff2
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-22 12:32:13 +0100

    Group models set up cont'd

---
+++ controllers/groups/groups.contollers.js
@@ -0,0 +1,9 @@
+const { Groups } = require("../../models/groups/groups.model");
+
+const createGroup = async (req, res) => {
+  try {
+    const { body } = req;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
