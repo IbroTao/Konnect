74f28a1bc7bfdce34dd02a8a3d9f478689fc0b98
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-22 21:58:00 +0100

    Group controller set up

--- controllers/groups/groups.contollers.js
+++ controllers/groups/groups.contollers.js
@@ -1,5 +1,6 @@
 const { Groups } = require("../../models/groups/groups.model");
 const myCustomLabels = require("../../utils/labelPaginate");
+const { validateMongoId } = require("../../utils/validateMongoId");
 
 const createGroup = async (req, res) => {
   try {
@@ -54,4 +55,27 @@ const queryGroups = async (req, res) => {
   } catch (error) {
     res.status(500).json(error);
   }
+};
+
+const updateGroupInfo = async (req, res) => {
+  const { id } = req.params;
+  validateMongoId(id);
+  try {
+    const group = await Groups.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!group) {
+      res.status(404).json({ error: "no group found" });
+    }
+
+    res.status(200).json({ message: "group info updated", group });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = {
+  createGroup,
+  queryGroups,
+  updateGroupInfo,
 };
