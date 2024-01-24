c3af85001ae5d1ed8722dfe36983328a04815568
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-24 01:20:55 +0100

    userModel 100% rewrite

---
+++ middlewares/validate.js
@@ -0,0 +1,1 @@
+const joi = require("joi");
--- models/user.model.js
+++ models/user.model.js
@@ -2,33 +2,87 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
       required: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
-      required: true,
-    },
-    mobile: {
-      type: Number,
-      maxlength: 11,
-      required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      mtrim: true,
     },
     password: {
       type: String,
       required: true,
+      trim: true,
     },
-    image: {
+    avatar: {
+      url: {
+        type: String,
+      },
+      publicId: {
+        type: String,
+      },
+    },
+    bio: {
+      type: String,
+      minlength: 50,
+      maxlength: 250,
+    },
+    dob: {
+      type: Date,
+    },
+    location: {
       type: String,
     },
-    isVerified: {
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+    username: {
+      type: String,
+      minlength: 2,
+      unique: [true, "username already used by another user"],
+    },
+    totalFollowers: {
+      type: Number,
+      default: 0,
+    },
+    totalFollowing: {
+      type: Number,
+      default: 0,
+    },
+    allowDirectMessaging: {
       type: Boolean,
       default: false,
     },
-    isBlocked: {
+    accountType: {
+      type: String,
+      enum: ["basic", "preminum"],
+      default: "prenium",
+    },
+    points: {
+      type: Number,
+      default: 0,
+    },
+    interests: [
+      {
+        type: String,
+        enum: categories,
+      },
+    ],
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isSuspended: {
       type: Boolean,
       default: false,
     },
@@ -37,20 +91,17 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      default: "user",
+      enum: ["reader", "author", "both", "admin"],
+      default: "both",
     },
-    groups: Array,
-    refreshToken: {
+    permissions: {
+      type: [String],
+      enum: adminPermissions,
+    },
+    status: {
       type: String,
-    },
-    passwordChangedAt: {
-      type: Date,
-    },
-    passwordResetToken: {
-      type: String,
-    },
-    passwordResetTokenExpiresAt: {
-      type: Date,
+      enum: ["online", "offline"],
+      default: "offline",
     },
   },
   {
