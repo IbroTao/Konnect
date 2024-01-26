119c343e667595f826bc0f96d4b690afe8c4eddc
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-26 12:02:50 +0100

    .env set up

--- configs/config.js
+++ configs/config.js
@@ -1,3 +1,5 @@
 const dotenv = require("dotenv");
 const path = require("path");
 const Joi = require("joi");
+
+dotenv.config({ path: path.join(__dirname) });
--- services/auth.service.js
+++ services/auth.service.js
@@ -4,3 +4,5 @@ const userService = require("./user.service");
 const { tokenTypes } = require("../configs/tokenTypes");
 const Token = require("../models/token.model");
 const ApiError = require("../utils/ApiError");
+const { User } = require("../models");
+const { uniqueFiveDigits } = require("../utils/generateFiveDigits");
