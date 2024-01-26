af2dd8e29f47f33006e25613f8e13a8ba94c2eea
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-26 12:14:29 +0100

    Feat: .env configured with Joi

--- configs/config.js
+++ configs/config.js
@@ -2,4 +2,11 @@ const dotenv = require("dotenv");
 const path = require("path");
 const Joi = require("joi");
 
-dotenv.config({ path: path.join(__dirname) });
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+const envSchema = Joi.object().keys({
+  NODE_ENV: Joi.string().valid("production", "test", "development").required(),
+  PORT: Joi.number().default(3000),
+  MONGO_URL: Joi.string().required().description("MongoDB URL"),
+  JWT_SECRET: Joi.string().required().description("JWT Secret Key"),
+});
