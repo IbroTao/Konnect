fa00611557cd1285e52a22141da441073499fa40
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 09:06:04 +0100

    Services: emailService cont'd

--- services/email.service.js
+++ services/email.service.js
@@ -1,9 +1,13 @@
 const nodemailer = require("nodemailer");
-const config = require('../configs/config');
-const logger = require('../configs/logger');
-const {forgetPasswordTemplate, updatedPasswordTemplate, verifyAccountViaDigitsTemplate} = require('../utils/convertMjmlToHtml');
+const config = require("../configs/config");
+const logger = require("../configs/logger");
+const {
+  forgetPasswordTemplate,
+  updatedPasswordTemplate,
+  verifyAccountViaDigitsTemplate,
+} = require("../utils/convertMjmlToHtml");
 
-const SMTP_HOST = 'smtp.gmail.com';
+const SMTP_HOST = "smtp.gmail.com";
 const SMTP_PORT = 465;
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -16,17 +20,48 @@ const transporter = nodemailer.createTransport({
     pass: "vast ffoy hdqi bdqa",
   },
   logger: false,
-  debug: true
+  debug: true,
 });
 
-const sendEmail = async(to, subject, text) => {
-    const msg = {from: config.email.from, to, subject, html: text};
-    await transporter.sendMail(msg) 
+const sendEmail = async (to, subject, text) => {
+  const msg = { from: config.email.from, to, subject, html: text };
+  await transporter.sendMail(msg);
 };
 
-const sendResetPasswordEmail = async(name, to, token) => {
-    const subject = 'Reset Password';
-    const resetPasswordUrl = `http:localhost:9090/konnect/reset-password?token=${token}`,
-    const text = `Dear ${name}, To rest your password, click on this link: ${resetPasswordUrl}. If you did not request any password resets, then kindly ignore this email or contact our team for more information.`;
-    await sendEmail(to, subject, text)
-}
+const sendResetPasswordEmail = async (name, to, token) => {
+  const subject = "Reset Password";
+  const resetPasswordUrl = `http://localhost:9090/konnect/reset-password?token=${token}`;
+  const text = `Dear ${name}, 
+    To rest your password, click on this link: ${resetPasswordUrl}. If you did not request any password resets, then kindly ignore this email or contact our team for more information.`;
+  await sendEmail(to, subject, text);
+};
+
+const sendVerificationEmail = async (name, to, token) => {
+  const subject = "Email Verification";
+  const verificationEmailUrl = `http://localhost:9090/konnect/verify-email?token=${token}`;
+  const text = `Dear ${name},
+    To verify your email, click on this link: ${verificationEmailUrl}
+    If you did not create an account, kindly ignore this email or contact our team for more information`;
+};
+
+const sendFiveDigitsForVerification = async (to) => {
+  const subject = "Verify Your Account";
+  const from = `ghoscodert@gmail.com`;
+  var mailOptions = {
+    from: from,
+    to: to,
+    subject: `Verify Your Email`,
+    html: `<p>oo<p>`,
+  };
+  try {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
