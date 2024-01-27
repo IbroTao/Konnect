aa2dbad651ade8268b8823f745b9c862e63eda5b
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 09:23:17 +0100

    Middlewares: errorHandler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+const httpStatus = require("http-status");
+const config = require("../configs/config");
+const logger = require("../configs/logger");
+const ApiError = require("../utils/ApiError");
