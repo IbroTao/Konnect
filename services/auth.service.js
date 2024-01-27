8e892aeab8f91fe1095c63c03962ff289c473ea0
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 15:24:32 +0100

    Services: authService - refreshAuth

--- services/auth.service.js
+++ services/auth.service.js
@@ -36,8 +36,14 @@ const logout = async (refreshToken) => {
 
 const refreshAuth = async (refreshToken) => {
   try {
+    const refreshTokenDoc = await tokenService.verifyToken(
+      refreshToken,
+      tokenTypes.REFRESH
+    );
+    const user = await userService.getUserById(refreshTokenDoc.user);
+    if (!user) throw new Error();
   } catch (error) {
-    throw new ApiError(httpsStatus.UNAUTHORIZED, "please authenticate");
+    throw new ApiError(httpStatus.UNAUTHORIZED, "please authenticate");
   }
 };
 
