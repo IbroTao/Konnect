c139ac6a102ab2d348a46c8aceaa5d3accd1c13a
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 13:17:05 +0100

    Utils: myCustomLAbels

---
+++ utils/myCustomLabels.js
@@ -0,0 +1,11 @@
+const myCustomLabels = {
+  totalDocs: "totalItems",
+  docs: "data",
+  limit: "perPage",
+  page: "currentPage",
+  nextPage: "nextPage",
+  prevPage: "prevPage",
+  totalPages: "pageCount",
+};
+
+module.exports = myCustomLabels;
