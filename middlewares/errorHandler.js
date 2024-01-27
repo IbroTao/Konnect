684eba349750c1b843dcb56acbbd3b8f4ca50240
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 09:30:41 +0100

    Middlewares: errorHandler set up

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,3 +3,20 @@ const httpStatus = require("http-status");
 const config = require("../configs/config");
 const logger = require("../configs/logger");
 const ApiError = require("../utils/ApiError");
+
+const errorConverter = async (err, req, res, next) => {
+  let error = err;
+  if (!(error instanceof ApiError)) {
+    const statusCode =
+      error.statusCode || error instanceof mongoose.Error
+        ? httpStatus.BAD_REQUEST
+        : httpStatus.INTERNAL_SERVER_ERROR;
+    const message = error.message || httpStatus[statusCode];
+    error = new ApiError(statusCode, message, false, err.stack);
+  }
+  next(error);
+};
+
+module.exports = {
+  errorConverter,
+};
