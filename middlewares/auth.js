a2a538cdbbf19ffb429e06d708389cfcd8079fd4
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 09:53:11 +0100

    Middlewares: auth

---
+++ middlewares/auth.js
@@ -0,0 +1,4 @@
+const passport = require("passport");
+const httpStatus = require("http-status");
+const ApiError = require("../utils/ApiError");
+const { roleRights } = require("../configs/roles");
