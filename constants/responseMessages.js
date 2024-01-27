b419d6028420cf45d6a2ad10b7fb53e33c91fcf9
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 12:19:05 +0100

    Constants: responseMessages

---
+++ constants/responseMessages.js
@@ -0,0 +1,10 @@
+const MESSAGES = {
+  PASSWORD_RESET: "password reset successfully",
+  SEND_VERIFICATION_CODE: "verification code sent",
+  USER_NOT_FOUND: "user not found",
+  RESOURCE_MISSING: "resource not found",
+};
+
+module.exports = {
+  MESSAGES,
+};
