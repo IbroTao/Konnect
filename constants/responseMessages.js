50b5200dcc5b1ce90a6433ba05741b6b2175d09f
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 12:45:29 +0100

    Constants: responseMessages cont'd

--- constants/responseMessages.js
+++ constants/responseMessages.js
@@ -3,6 +3,11 @@ const MESSAGES = {
   SEND_VERIFICATION_CODE: "verification code sent",
   USER_NOT_FOUND: "user not found",
   RESOURCE_MISSING: "resource not found",
+  USER_NOT_FOUND: "user not found",
+  UPDATE_PASSWORD: "password updated",
+  CANNOT_SET_PASSWORD: "cannot set user's username and password",
+  EMAIL_VERIFIED: "email verified",
+  CODE_EXPIRED: "either code has expired or something went wrong",
 };
 
 module.exports = {
