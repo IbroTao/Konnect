e419bc047d4bed10b7615cd3d401948d447d939a
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 11:01:07 +0100

    Services: index file for all services

---
+++ services/index.js
@@ -0,0 +1,4 @@
+module.exports.userService = require("./user.service");
+module.exports.emailService = require("./email.service");
+module.exports.authService = require("./auth.service");
+module.exports.tokenService = require("./token.service");
