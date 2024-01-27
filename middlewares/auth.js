6c98342d0655bb504db64fc10e6818b67b3d7ff0
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 10:03:49 +0100

    Middlewares: auth - verifyCallback

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,3 +2,28 @@ const passport = require("passport");
 const httpStatus = require("http-status");
 const ApiError = require("../utils/ApiError");
 const { roleRights } = require("../configs/roles");
+
+const verifyCallback =
+  (req, resolve, reject, requiredRights) => async (err, user, info) => {
+    if (err || user || info) {
+      return reject(
+        new ApiError(httpStatus.UNAUTHORIZED, "Please authenticate")
+      );
+    }
+    req.user = user;
+
+    if (requiredRights.length) {
+      const userRights = roleRights.get(user.role);
+      const permissionRights = user.permissions;
+      const hasRequiredRights = requiredRights.every(
+        (requiredRights) =>
+          userRights.includes(requiredRights) &&
+          permissionRights.every((value) => requiredRights.includes(value))
+      );
+      if (!hasRequiredRights && req.params.userId !== user.id) {
+        return reject(new ApiError(httpStatus.FORBIDDEN, "Forbidden"));
+      }
+    }
+
+    resolve();
+  };
