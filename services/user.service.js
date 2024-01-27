5ce422657f212fcca9400d0bee717bb93ee7d765
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 12:58:36 +0100

    Services: userService - isUsernameTaken

--- services/user.service.js
+++ services/user.service.js
@@ -1,3 +1,13 @@
 const httpStatus = require("http-status");
 const { User, Follow, deletedAccount } = require("../models");
 const ApiError = require("../utils/ApiError");
+const { MESSAGES } = require("../constants/responseMessages");
+
+const isUsernameTaken = async (username) => {
+  const user = await User.findOne({ username });
+  if (user) throw new ApiError(httpStatus.BAD_REQUEST, "username taken");
+  if (user && !user.username) {
+    return "available";
+  }
+  return "available";
+};
