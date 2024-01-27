d14d13feba301a9ff2e9cbfb1af584424c1f4f77
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-27 13:28:44 +0100

    Services: userService -queryUsers

--- services/user.service.js
+++ services/user.service.js
@@ -2,6 +2,7 @@ const httpStatus = require("http-status");
 const { User, Follow, deletedAccount } = require("../models");
 const ApiError = require("../utils/ApiError");
 const { MESSAGES } = require("../constants/responseMessages");
+const myCustomLabels = require("../utils/myCustomLabels");
 
 const isUsernameTaken = async (username) => {
   const user = await User.findOne({ username });
@@ -25,7 +26,6 @@ const getUserByUsername = async (username) => {
     "username",
     "email",
     "avatar",
-    "accountType",
     "location",
     "bio",
     "gender",
@@ -33,4 +33,45 @@ const getUserByUsername = async (username) => {
     "totalFollowings",
   ]);
   return user;
+};
+
+const queryUsers = async (
+  { search, filter },
+  { limit, page, orderBy, sortedBy }
+) => {
+  const options = {
+    lean: true,
+    customLabels: myCustomLabels,
+  };
+  const users = await Users.paginate(
+    {
+      $or: [
+        { name: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+        { role: { $regex: search, $options: "i" } },
+      ],
+      ...filter,
+    },
+    {
+      ...(limit ? { limit } : { limit: 10 }),
+      ...page,
+      sort: { [orderBy]: sortedBy === "asc" ? 1 : -1 },
+      ...options,
+      select: [
+        "-password",
+        "-dob",
+        "-showMatureContent",
+        "-accountType",
+        "-updatedAt",
+        "__v",
+      ],
+    }
+  );
+  return users;
+};
+
+module.exports = {
+  createUser,
+  getUserByUsername,
+  queryUsers,
 };
