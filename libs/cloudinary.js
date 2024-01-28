ea5747c0e50948c4e1e7c538329e13de737a2775
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-28 08:03:13 +0100

    Libs: cloudinary - uploadSingle

--- libs/cloudinary.js
+++ libs/cloudinary.js
@@ -5,3 +5,12 @@ const {cloudinary} = require('../configs/config');
 require('dotenv').config({path: path.resolve(process.cwd), '.env'});
 
 cloud.v2.config(cloudinary);
+
+const uploadSingle = async(filePath) => {
+    const {secure_url, public_id} = await cloud.v2.uploader.upload(filePath);
+    return {url: secure_url, publicId: public_id}
+};
+
+module.exports = {
+    uploadSingle
+}
