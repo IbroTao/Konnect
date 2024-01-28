21febd780fe09dc437cf4274e8cd50e35595e5ee
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-28 09:00:45 +0100

    Models: utils - notificatonInfo

---
+++ models/utils/noificationInfo.js
@@ -0,0 +1,17 @@
+const notificationInfo = {
+  likedImage: "your image was liked by",
+  requestImage: "user",
+  userImage: "user",
+  groupImage: "group",
+  likeImage: "thumb",
+  postImage: "post",
+  commentImage: "chat",
+  postComment: "commented on your post",
+  followed: "has followed you",
+  likePost: "liked your post",
+  addAdmin: "you have been made an admin in",
+  groupRequest: "sent a request to join",
+  groupRequestAccepted: "has accepted your request to join",
+};
+
+module.exports = { notificationInfo };
