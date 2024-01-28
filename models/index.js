8ad702a9f5145c56cf1ec59360ea1633d36b0871
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-28 10:08:21 +0100

    Schemas: notificationQueue

--- models/index.js
+++ models/index.js
@@ -2,5 +2,3 @@ module.exports.User = require("./user.model");
 module.exports.Token = require("./token.model");
 module.exports.Follow = require("./follow.model");
 module.exports.deletedAccount = require("./deleteAccounts.model");
-module.exports.userFeed = require("./utils/userFeed");
-module.exports.notificationInfo = require("./utils/notificationInfo");
--- services/index.js
+++ services/index.js
@@ -2,3 +2,5 @@ module.exports.userService = require("./user.service");
 module.exports.emailService = require("./email.service");
 module.exports.authService = require("./auth.service");
 module.exports.tokenService = require("./token.service");
+module.exports.userFeed = require("./utils/userFeed");
+module.exports.notificationInfo = require("./utils/notificationInfo");
