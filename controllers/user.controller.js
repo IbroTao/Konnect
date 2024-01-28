c5a3468b7ec5bcb9b733bcc5aee6084949c77f35
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-28 10:30:05 +0100

    Controllers: userController - comparePassword

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,24 @@
 const httpStatus = require("http-status");
 const pick = require("../utils/pick");
 const ApiError = require("../utils/ApiError");
-const catchSync = require("../utils/catchAsync");
+const catchAsync = require("../utils/catchAsync");
 const { uploadSingle } = require("../libs/cloudinary");
-const { userService } = require("../services");
+const { userService, userFeed, notificationInfo } = require("../services");
+const { MESSAGES } = require("../constants/responseMessages");
+const otherConstants = require("../constants/others");
+
+const comparePassword = catchAsync(async (req, res) => {
+  if (!req.body.password)
+    throw new ApiError(httpStatus.BAD_REQUEST, "provide password");
+  const passwordMatch = await userService.comparePassword(
+    req.body.password,
+    req.user.id
+  );
+  if (!passwordMatch)
+    throw new ApiError(httpStatus.BAD_REQUEST, MESSAGES.PASSWORD_NO_MATCH);
+  res.status(204).json();
+});
+
+module.exports = {
+  comparePassword,
+};
