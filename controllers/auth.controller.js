9d1ea82e48cab3417665b4f9b6df64690246290b
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-30 00:51:15 +0100

    Controllers: authController - register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,3 +9,15 @@ const {
 const ApiError = require("../utils/ApiError");
 const { MESSAGES } = require("../constants/responseMessages");
 const { defaultEmailSender } = require("../services/email.service");
+
+const register = catchAsync(async (req, res) => {
+  const { username } = req.body;
+  const user = await userService.createUser({
+    ...req.body,
+    username: `@${username}`,
+  });
+  await authService.getVerificationCode(req, user);
+  res.status(httpStatus.CREATED).json({ user });
+});
+
+module.exports = { register };
