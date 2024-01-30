972795f69a3c0647cfd0e3046f482a9767a24d5f
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-30 12:28:23 +0100

    Models: Groups - groupModel cont'd

--- models/groups/group.model.js
+++ models/groups/group.model.js
@@ -2,3 +2,20 @@ const mongoose = require("mongoose");
 const { toJSON } = require("../plugins");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const modelNames = require("../../constants/modelNames");
+
+const memberSchema = new mongoose.Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: modelNames.user,
+    },
+    joinedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    id: false,
+    _id: false,
+  }
+);
