37d2c4266ac9293b342265093bc3aa39d1980f71
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-30 12:25:14 +0100

    Models: Groups - groupModel

---
+++ models/groups/group.model.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const { toJSON } = require("../plugins");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const modelNames = require("../../constants/modelNames");
