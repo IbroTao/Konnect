c19d748e55e29572c8b149cb875ed636cdc18fa0
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-30 01:01:50 +0100

    Validations: customValidation cont'd

--- validations/custom.validation.js
+++ validations/custom.validation.js
@@ -7,6 +7,16 @@ const objectId = (value, helpers) => {
 
 const password = (value, helpers) => {
   if (value.length < 8) {
-    return helpers.message("");
+    return helpers.message("password must be at least 8 characters");
   }
+  if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+    return helpers.message(
+      "password must contain at least 1 letter and 1 number"
+    );
+  }
+};
+
+module.exports = {
+  objectId,
+  password,
 };
