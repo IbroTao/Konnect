2dd03c0f9969c2f0321a611225b65b4f7893abcc
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-31 17:50:33 +0100

    Models: Groups - postModel cont'd

--- models/groups/posts.model.js
+++ models/groups/posts.model.js
@@ -4,3 +4,26 @@ const likeSchema = require("../schemas/like.schema");
 const shareSchema = require("../schemas/share.schema");
 const modelNames = require("../../constants/modelNames");
 const toJSON = require("../plugins/toJSON.plugin");
+
+const schema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 1,
+  },
+  groupId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: modelNames.group,
+  },
+  file: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  commentCount: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: modelNames.user,
+    required: true,
+  },
+});
