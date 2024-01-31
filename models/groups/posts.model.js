0c09f93b73428936c465239664bbe90438d49822
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-31 13:11:19 +0100

    Models: Groups - postModel cont'd

--- models/groups/posts.model.js
+++ models/groups/posts.model.js
@@ -2,3 +2,5 @@ const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const likeSchema = require("../schemas/like.schema");
 const shareSchema = require("../schemas/share.schema");
+const modelNames = require("../../constants/modelNames");
+const toJSON = require("../plugins/toJSON.plugin");
