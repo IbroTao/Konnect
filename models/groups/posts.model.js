2512f1c0989aa2e8edadafb72cb8434c72645f5f
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-31 13:09:21 +0100

    Models: Groups - postModel

---
+++ models/groups/posts.model.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const likeSchema = require("../schemas/like.schema");
+const shareSchema = require("../schemas/share.schema");
