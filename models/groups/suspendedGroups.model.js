ac1ba053048c5d4c9b8c43d33eaca249052f9df5
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-01 09:52:38 +0100

    Models: Groups - suspendedGroupsModel cont'd

--- models/groups/suspendedGroups.model.js
+++ models/groups/suspendedGroups.model.js
@@ -3,3 +3,5 @@ const modelNames = require("../../constants/modelNames");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const toJSON = require("../plugins/toJSON.plugin");
 const httpStatus = require("http-status");
+const ApiError = require("../../utils/ApiError");
+const { toJSON } = require("../plugins");
