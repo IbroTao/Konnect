adad05a7518166a40bd84284dcc505301f8c071b
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-01 09:48:40 +0100

    Utils: dateCreator

---
+++ utils/dateCreator.js
@@ -0,0 +1,19 @@
+class DateCreator {
+  static add24hours(date) {
+    return date.setHours(date.getHours() + 24);
+  }
+
+  static add7days(date) {
+    return date.setDays(date.getDays() + 7);
+  }
+
+  static add14days(date) {
+    return date.setDays(date.getDays() + 14);
+  }
+
+  static add1months(date) {
+    return date.setMonth(date.getMonth() + 1);
+  }
+
+  static add6months;
+}
