16bb6e0b06ac1476ea405df6ddd0ac58a69b2c74
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-01 12:23:19 +0100

    Models: suspendedAccounts cont'd

--- models/suspendedAccounts.js
+++ models/suspendedAccounts.js
@@ -5,3 +5,30 @@ const modelNames = require("../constants/modelNames");
 const toJSON = require("./plugins/toJSON.plugin");
 const DateCreator = require("../utils/dateCreator");
 const ApiError = require("../utils/ApiError");
+
+const schema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: modelNames.user,
+    },
+    reason: {
+      type: String,
+    },
+    duration: {
+      type: String,
+      default: "0",
+      enum: ["24hours", "7days", "14days", "1month", "6months"],
+      required: true,
+    },
+    releaseDate: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+schema.plugin(mongoosePaginate);
+schema.plugin(toJSON);
