4bcd6c4eab8a36139100f2f91aab9b38bac339d1
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-01 09:56:28 +0100

    Models: Groups - suspendedGroupsModel cont'd

--- models/groups/suspendedGroups.model.js
+++ models/groups/suspendedGroups.model.js
@@ -5,3 +5,20 @@ const toJSON = require("../plugins/toJSON.plugin");
 const httpStatus = require("http-status");
 const ApiError = require("../../utils/ApiError");
 const { toJSON } = require("../plugins");
+
+const schema = new mongoose.Schema({
+  groupId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: modelNames.group,
+    required: true,
+  },
+  reason: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: String,
+    enum: ["24hours", "7days", "1month", "6months"],
+    default: "24hours",
+  },
+});
