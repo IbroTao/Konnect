247338e282ffb505c32bf778b047a0b29eb237ef
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-02 09:08:46 +0100

    Services: Group - groupService cont'd

--- services/groups/group.service.js
+++ services/groups/group.service.js
@@ -25,9 +25,37 @@ const getMembers = async (id) => {
   return members;
 };
 
+const queryGroups = async (
+  { search, filter },
+  { page, limit, orderBy, sortedBy }
+) => {
+  const options = {
+    lean: true,
+  };
+
+  const groups = await Group.paginate(
+    {
+      $or: [
+        { name: { $regex: search, $options: "i" } },
+        { info: { $regex: search, $options: "i" } },
+      ],
+      ...filter,
+    },
+    {
+      ...(limit ? { limit } : { limit: 5 }),
+      page,
+      sort: { [orderBy]: sortedBy === "asc" ? 1 : -1 },
+      ...options,
+      populate: { path: "admins.id", select: "_id username avatar name" },
+    }
+  );
+  return groups;
+};
+
 module.exports = {
   createGroup,
   getAGroupById,
   getGroupByName,
   getMembers,
+  queryGroups,
 };
