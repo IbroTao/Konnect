92a5c200b97248e8b045356e61a322dcf4600d3d
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-02 11:30:55 +0100

    Services: index.js

--- services/index.js
+++ services/index.js
@@ -4,3 +4,6 @@ module.exports.authService = require("./auth.service");
 module.exports.tokenService = require("./token.service");
 module.exports.userFeed = require("./utils/userFeed");
 module.exports.notificationInfo = require("./utils/notificationInfo");
+module.exports.groupService = require("./groups/group.service");
+module.exports.groupPostService = require("./groups/post.service");
+module.exports.groupCommentService = require("./groups/comment.service");
