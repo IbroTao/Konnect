2644200edf083947a3a62a535e50eb1097c95bbf
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-02 13:11:17 +0100

    Models: groupRooms - group.js cont'd

--- models/groupRooms/msg.js
+++ models/groupRooms/msg.js
@@ -2,3 +2,17 @@ const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const modelNames = require("../../constants/modelNames");
 const toJSON = require("../plugins/toJSON.plugin");
+
+const readBy = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: modelNames.user,
+      required: true,
+    },
+  },
+  {
+    _id: false,
+    timestamps: true,
+  }
+);
