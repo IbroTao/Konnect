c908fc70bff7cc361655f4356e7dec80b1dc2244
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-02 08:32:28 +0100

    Services: adminService - createAdmin

--- services/admin.service.js
+++ services/admin.service.js
@@ -5,4 +5,20 @@ const {
   suspendedGroups,
   suspendedAccounts,
 } = require("../models");
+const { userService } = require("../services");
 const myCustomLabels = require("../utils/myCustomLabels");
+
+const createAdmin = async ({ name, email, password, permissions }) => {
+  return userService.createUser({
+    name,
+    password,
+    email,
+    role: "admin",
+    isEmailVerirfied: true,
+    permissions,
+  });
+};
+
+module.exports = {
+  createAdmin,
+};
