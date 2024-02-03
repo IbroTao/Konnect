8713bc514400a02990177110252b18348c1b3c81
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-03 16:08:05 +0100

    Controller: groupController cont'd

--- controllers/groups/group.controller.js
+++ controllers/groups/group.controller.js
@@ -3,3 +3,10 @@ const ApiError = require("../../utils/ApiError");
 const catchAsync = require("../../utils/catchAsync");
 const { MESSAGES } = require("../../constants/responseMessages");
 const { uploadSingle, deleteSingle } = require("../../libs/cloudinary");
+const { groupService, notificationInfo } = require("../../services");
+
+const createGroup = catchAsync(async (req, res) => {
+  const { body } = req;
+  const isName = await groupService.getGroupByName(body.name);
+  if (isName) throw new ApiError(httpStatus);
+});
