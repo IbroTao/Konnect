ecb67ca9e9e88496a1b0ceb2a654f27badcc3583
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-03 16:19:18 +0100

    Controller: groupController cont'd

--- controllers/groups/group.controller.js
+++ controllers/groups/group.controller.js
@@ -8,5 +8,18 @@ const { groupService, notificationInfo } = require("../../services");
 const createGroup = catchAsync(async (req, res) => {
   const { body } = req;
   const isName = await groupService.getGroupByName(body.name);
-  if (isName) throw new ApiError(httpStatus);
+  if (isName) throw new ApiError(httpStatus.BAD_REQUEST, "name already taken");
+
+  await groupService.createGroup({
+    ...body,
+    admins: { id: [req.user._id] },
+    adminCount: 1,
+  });
+  if (!body)
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, MESSAGES.FAILURE);
+  res.status(201).json({ message: MESSAGES.SUCCESS });
 });
+
+module.exports = {
+  createGroup,
+};
