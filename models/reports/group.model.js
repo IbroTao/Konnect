cd54d78d683251565a04804709ba3e17dd3e85de
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-07 13:24:38 +0100

    Feat: groupModel created in Models: reports cont'd

--- models/reports/group.model.js
+++ models/reports/group.model.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
 const modelNames = require("../../constants/modelNames");
+
+const schema = new mongoose.Schema({
+  reporterId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: modelNames.user,
+    required: true,
+  },
+  reportedMsg: {},
+});
