5c4023172c408c101ca5cf3e8ca973439e36c875
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-07 13:54:07 +0100

    Feat: groupService set up in Services: initiateGroup

--- services/group.service.js
+++ services/group.service.js
@@ -1,1 +1,11 @@
 const modelNames = require("../constants/modelNames");
+const { Groups, GroupMsg } = require("../models");
+
+const initiateGroup = async (data) => {
+  const group = await Groups.create(data);
+  return group;
+};
+
+module.exports = {
+  initiateGroup,
+};
