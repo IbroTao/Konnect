317162967019ae6a9a95fadbe0aa9eb73f5ee1e7
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-12 11:38:36 +0100

    userValidation - getUsers

---
+++ validations/user.validation.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+const { password, objectId } = require("./custom.validation");
+
+const getUsers = {
+  query: Joi.object().keys({
+    name: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
