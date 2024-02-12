d7e6db6853ab619462214f1335047bbc58c4d17e
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-12 16:22:46 +0100

    userRoutes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,3 +3,15 @@ const validate = require("../middlewares/validate");
 const userValidation = require("../validations/user.validation");
 const userController = require("../controllers/user.controller");
 const validateAccount = require("../middlewares/validateUser");
+const { singleUpload } = require("../libs/multer");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(validateAccount, userController.getUsers)
+  .put(
+    validateAccount,
+    validate(userValidation.updateProfile),
+    userController.updateProfile
+  );
