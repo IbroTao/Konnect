953cf3252cc41811f1f85f8bd5e29ee12b67fa41
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-13 17:56:41 +0100

    Tokens: refreshToken & accessToken cont'd

--- configs/generateTokens.js
+++ configs/generateTokens.js
@@ -2,6 +2,9 @@ const jwt = require("jsonwebtoken");
 const { tokenTypes } = require("../configs/tokenTypes");
 const { Token } = require("../models");
 const moment = require("moment");
+const ApiError = require("../utils/ApiError");
+const httpStatus = require("http-status");
+const userService = require("../services/user.service");
 
 const generateToken = async (
   userId,
@@ -48,6 +51,59 @@ const generateAuthTokens = async (user) => {
     accessTokenExpires,
     tokenTypes.ACCESS
   );
+
+  const refreshTokenExpires = moment().add(30, "days");
+  const refreshToken = generateToken(
+    user.id,
+    refreshTokenExpires,
+    tokenTypes.REFRESH
+  );
+
+  await saveToken(
+    refreshToken,
+    user.id,
+    refreshTokenExpires,
+    tokenTypes.REFRESH
+  );
+
+  return {
+    access: {
+      token: accessToken,
+      expires: accessTokenExpires.toDate(),
+    },
+    refresh: {
+      token: refreshToken,
+      expires: refreshTokenExpires.toDate(),
+    },
+  };
+};
+
+const generateResetPasswordToken = async (email) => {
+  const user = await userService.getUserByEmail(email);
+  if (!user)
+    throw new ApiError(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      "No user found with this email"
+    );
+
+  const expires = moment().add(10, "minutes");
+  const resetPasswordToken = generateToken(
+    user.id,
+    expires,
+    tokenTypes.RESET_PASSWORD
+  );
+  await saveToken(
+    resetPasswordToken,
+    user.id,
+    expires,
+    tokenTypes.RESET_PASSWORD
+  );
+  return resetPasswordToken;
+};
+
+const generateVerifyEmailToken = async (email) => {
+  const user = await userService.getUserByEmail(email);
+  if (!user) throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR);
 };
 
 module.exports = { generateAuthTokens, generateToken, saveToken, verifyToken };
