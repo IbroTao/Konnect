c548bd45d47d3963ecae5c37e3d5a890128ba1e0
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-13 17:13:52 +0100

    Tokens: refreshToken & accessToken cont'd

--- configs/generateTokens.js
+++ configs/generateTokens.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { tokenTypes } = require("../configs/tokenTypes");
+const { Token } = require("../models");
 
 const generateAccessToken = async (
   userId,
@@ -16,6 +17,27 @@ const generateAccessToken = async (
 
 const verifyToken = async (token, type) => {
   const payload = jwt.verify(token, process.env.SECRET);
+  const verifyTok = await Token.findOne({
+    token,
+    type,
+    user: payload.sub,
+    blacklisted: false,
+  });
+  if (!verifyTok) {
+    throw new Error("Token not found");
+  }
+  return verifyTok;
+};
+
+const saveToken = async (token, userId, type, blacklisted = false) => {
+  const tokenDoc = await Token.create({
+    token,
+    type,
+    blacklisted,
+    user: userId,
+  });
+  if (!tokenDoc) throw new Error("Token not saved");
+  return tokenDoc;
 };
 
 module.exports = { generateAccessToken, generateRefreshToken };
