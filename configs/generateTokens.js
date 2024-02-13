a6126c102bb882c9bea5f13a24c1c36f0dcaef28
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-13 17:01:30 +0100

    Tokens: refreshToken & accessToken cont'd

--- configs/generateTokens.js
+++ configs/generateTokens.js
@@ -1,15 +1,21 @@
 const jwt = require("jsonwebtoken");
+const { tokenTypes } = require("../configs/tokenTypes");
 
-const generateAccessToken = (id) => {
-  return jwt.sign({ id }, process.env.SESSION_SECRET, {
-    expiresIn: "1d",
-  });
+const generateAccessToken = async (
+  userId,
+  type,
+  secret = process.env.SESSION_SECRET
+) => {
+  const payload = {
+    sub: userId,
+    exp: "1d",
+    type,
+  };
+  return jwt.sign(payload, secret);
 };
 
-const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.SESSION_SECRET, {
-    expiresIn: "3d",
-  });
+const verifyToken = async (token, type) => {
+  const payload = jwt.verify(token, process.env.SECRET);
 };
 
 module.exports = { generateAccessToken, generateRefreshToken };
