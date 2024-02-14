138a191022dd9358de206cecb8c76c64d33433ae
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-14 15:14:05 +0100

    feat: Routes for Community set up--

--- routes/community/community.route.js
+++ routes/community/community.route.js
@@ -1,2 +1,4 @@
 const express = require("express");
 const validate = require("../../middlewares/validate");
+const {} = require("../../validations");
+const { singleUpload, multipleUpload } = require("../../libs/multer");
