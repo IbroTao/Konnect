42ae829c45e8e9a38cdf9b1f2789b25fd48dd5b2
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-14 15:15:26 +0100

    feat: Routes for Community set up--

--- routes/community/community.route.js
+++ routes/community/community.route.js
@@ -2,3 +2,6 @@ const express = require("express");
 const validate = require("../../middlewares/validate");
 const {} = require("../../validations");
 const { singleUpload, multipleUpload } = require("../../libs/multer");
+const validateAccount = require("../../middlewares/validateUser");
+
+const router = express.Router();
