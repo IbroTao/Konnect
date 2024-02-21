6ae6746b17c54edcdef296b9738220ae7b541fab
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-21 06:16:23 +0100

    Feat: comment feature implementated for community cont'd

--- controllers/community/comment.controller.js
+++ controllers/community/comment.controller.js
@@ -9,6 +9,21 @@ const { notificationQueue } = require("../../schemas");
 
 const submitComment = catchAsync(async (req, res) => {
   const { body, user } = req;
+
+  const comment = await communityCommentService.createComment(
+    body.postId,
+    body.content,
+    user._id,
+    body.parentId || null
+  );
+
+  if (!comment)
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, MESSAGES.FAILURE);
+
+  notificationQueue.msg = `${req.user.username} commented on your post`;
+  notificationQueue.type = "comment";
+  notificationQueue.timestamp = new Date().toISOString();
+  notificationQueue.senderId = comment.author;
 });
 
 module.exports = {
