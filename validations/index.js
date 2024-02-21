0e20a8477f64a26fafd671713f44b7c5b122aeae
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-21 06:52:22 +0100

    Feat: index file for validations

---
+++ validations/index.js
@@ -0,0 +1,4 @@
+module.exports.authValidation = require("./auth.validation");
+module.exports.userValidation = require("./user.validation");
+module.exports.groupValidation = require("./group.validation");
+module.exports.communityValidation = require("./community.validation");
