e4d29caa36a18eafbb4563a159672bdf40f72a2f
IbroTao <ibrahimtaofeek005@gmail.com>
2024-02-21 05:37:41 +0100

    Feat: validations for group

--- validations/group.validation.js
+++ validations/group.validation.js
@@ -3,53 +3,11 @@ const { objectId } = require("./custom.validation");
 
 const createGroup = {
   body: Joi.object().keys({
-    title: Joi.string().required(),
-    category: Joi.array().required(),
-    mature: Joi.boolean(),
-    description: Joi.string(),
-    type: Joi.string(),
-    language: Joi.string(),
-    tags: Joi.string(),
-    isUploaded: Joi.boolean(),
-  }),
-};
-
-const getPosts = {
-  query: Joi.object().keys({
-    title: Joi.string(),
-    sortBy: Joi.string(),
-    limit: Joi.number().integer(),
-    page: Joi.number().integer(),
-  }),
-};
-
-const createComment = {
-  body: Joi.object().keys({
-    bookId: Joi.string().required(),
-    parentId: Joi.string(),
-    content: Joi.string().required(),
-  }),
-};
-
-const getPost = {
-  params: Joi.object().keys({
-    bookId: Joi.string().custom(objectId),
-  }),
-};
-
-const updatePost = {
-  params: Joi.object().keys({
-    bookId: Joi.string().custom(objectId),
-  }),
-  body: Joi.object().keys({
-    title: Joi.string().required(),
+    name: Joi.string().min(3).max(40).required(),
+    members: Joi.array(),
   }),
 };
 
 module.exports = {
   createGroup,
-  getPosts,
-  createComment,
-  getPost,
-  updatePost,
 };
