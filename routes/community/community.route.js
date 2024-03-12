d8c4c6f2d25d9e38e0b283286333e3895478f54f
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-12 11:20:22 +0100

    Feat: community routes set up

--- routes/community/community.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const validate = require("../../middlewares/validate");
-const {} = require("../../validations");
-const { singleUpload, multipleUpload } = require("../../libs/multer");
-const validateAccount = require("../../middlewares/validateUser");
-
-const router = express.Router();
-
-router.post("/new", validateAccount, validate(communityValidation));
--- routes/community/community.routes.js
+++ routes/community/community.routes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const validate = require("../../middlewares/validate");
-const {} = require("../../validations");
+const { communityValidation } = require("../../validations");
 const { singleUpload, multipleUpload } = require("../../libs/multer");
 const validateAccount = require("../../middlewares/validateUser");
+const {} = require("../../controllers");
 
 const router = express.Router();
 
-router.post("/new", validateAccount, validate(communityValidation));
+router.post(
+  "/new",
+  validateAccount,
+  validate(communityValidation),
+  communityController
+);
