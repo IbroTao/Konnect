d54d0dded9dd6908e2d06cfb624553081784522d
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-23 18:33:02 +0100

    Data: tempStore

---
+++ data/tempStore.js
@@ -0,0 +1,3 @@
+const tempStore = [];
+
+module.exports = { tempStore };
---
+++ services/notifications.service.js
@@ -0,0 +1,4 @@
+const httpStatus = require("http-status");
+const { Notification } = require("../models");
+const ApiError = require("../utils/ApiError");
+const myCustomLabels = require("../utils/myCustomLabels");
