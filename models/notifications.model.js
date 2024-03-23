3f306180f5800801625612a8e1ffc19eb4345305
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-23 18:20:25 +0100

    Feat: models for notification

--- models/notifications.model.js
+++ models/notifications.model.js
@@ -1,5 +1,17 @@
-const mongoose = require('mongoose');
-const modelNames = require('../constants/modelNames');
-const mongoosePaginate = require('mongoose-paginate-v2');
+const mongoose = require("mongoose");
+const modelNames = require("../constants/modelNames");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const 
+const notificationSchema = new mongoose.Schema({
+  image: {
+    type: String,
+  },
+  message: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  link: {
+    type: String,
+  },
+});
