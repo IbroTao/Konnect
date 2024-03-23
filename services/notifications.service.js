30ead50ad443b7fc1f9b70a6847fb6b04df928ee
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-23 18:37:48 +0100

    Data: tempStore

--- services/notifications.service.js
+++ services/notifications.service.js
@@ -2,3 +2,22 @@ const httpStatus = require("http-status");
 const { Notification } = require("../models");
 const ApiError = require("../utils/ApiError");
 const myCustomLabels = require("../utils/myCustomLabels");
+
+const createNotification = async ({ image, link, message, userId }) => {
+  if (!image && !link && !message && !userId) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      "cannot send notification with incomplete fields"
+    );
+  }
+  return Notification.create({
+    image,
+    link,
+    message,
+    userId,
+  });
+};
+
+module.exports = {
+  createNotification,
+};
